d585cfe3c32c8695c272dc923302df53f7a90df1
OdedRosen <oded219@gmail.com>
2021-03-12 21:54:05 -0500

    update

--- sick-fits/backend/seed-data/index.ts
+++ sick-fits/backend/seed-data/index.ts
@@ -1,17 +1,20 @@
+import { KeystoneContext } from '@keystone-next/types';
+import { Mongoose } from 'mongoose';
 import { products } from './data';
 
-export async function insertSeedData(keystone: any) {
+export async function insertSeedData(keystone: KeystoneContext): Promise<void> {
   console.log(`🌱 Inserting Seed Data: ${products.length} Products`);
-  const { mongoose } = keystone.adapters.MongooseAdapter;
+  const mongoose = keystone.mongoose as Mongoose;
   for (const product of products) {
     console.log(`  🛍️ Adding Product: ${product.name}`);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const { _id } = await mongoose
-    .model('ProductImage')
-    .create({ image: product.photo, altText: product.description });
+      .model('ProductImage')
+      .create({ image: product.photo, altText: product.description });
     product.photo = _id;
     await mongoose.model('Product').create(product);
   }
   console.log(`✅ Seed Data Inserted: ${products.length} Products`);
-  console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+  console.log('👋 Please start the process with `yarn dev` or `npm run dev`');
   process.exit();
 }
